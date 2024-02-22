22ebc95d02ca83de94f75d638c466444f47088b6
Ajay Choudhary <ajay.choudhary@brainvire.com>
2024-02-22 20:13:16 +0530

    Worked on Graphql with apollo client using json demo api

---
+++ TestData.tsx
@@ -0,0 +1,35 @@
+import { useQuery, gql } from "@apollo/client";
+
+const TestData: React.FC = () => {
+  const GET_POSTS = gql`
+    query {
+      posts {
+        data {
+          id
+          title
+          body
+        }
+      }
+    }
+  `;
+  const { data, loading, error } = useQuery(GET_POSTS);
+  console.log(data, "data");
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  const post = data.post;
+  return (
+    <div>
+      <h1>Posts</h1>
+      {data.posts.data.map((post: any) => (
+        <div key={post.id}>
+          <h2>{post.id}</h2>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TestData;
