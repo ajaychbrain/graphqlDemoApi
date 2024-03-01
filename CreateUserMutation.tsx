8fbd1676c557c6189cbd0e04019f53ddf75963f7
Ajay Choudhary <ajay.choudhary@brainvire.com>
2024-03-01 19:55:36 +0530

    Worked on demo project added validation using formik yup, Search bar functionality and Pagination Functionality

--- CreateUserMutation.tsx
+++ CreateUserMutation.tsx
@@ -1,8 +1,17 @@
 import { gql, useMutation } from "@apollo/client";
 import { Button, TextField, CircularProgress } from "@mui/material";
 import React, { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import UpdateUserMutation from "./UpdateUserMutation";
 import DeleteUserMutation from "./DeleteUserMutation";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import DirectionsIcon from "@mui/icons-material/Directions";
 
 const CREATE_USER_MUTATION = gql`
   mutation CreatePost($input: CreatePostInput!) {
@@ -24,59 +33,69 @@ const UPDATE_USER_MUTATION = gql`
   }
 `;
 
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required("Title is required"),
+  body: Yup.string().required("Body is required"),
+});
+
 const CreateUserMutation = () => {
   const [createUser, { loading: createLoading, error: createError }] =
     useMutation(CREATE_USER_MUTATION);
   const [updateUser, { loading: updateLoading, error: updateError }] =
     useMutation(UPDATE_USER_MUTATION);
   const [formData, setFormData] = useState<any[]>([]);
-
   const [editIndex, setEditIndex] = useState(null);
   const [showLoader, setShowLoader] = useState(false);
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 4; // Show 4 records per page
 
-  const handleSubmit = async (event: any) => {
-    event.preventDefault();
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      body: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      try {
+        setShowLoader(true);
 
-    const input = {
-      title: event.target.title.value,
-      body: event.target.body.value,
-    };
-
-    try {
-      setShowLoader(true);
-
-      if (editIndex !== null) {
-        const postId = formData[editIndex].id;
-        await updateUser({ variables: { id: postId, input } });
-        const updatedFormData: any = [...formData];
-        updatedFormData[editIndex] = {
-          ...updatedFormData[editIndex],
-          ...input,
+        const input = {
+          title: values.title,
+          body: values.body,
         };
-        setFormData(updatedFormData);
-        setEditIndex(null);
-      } else {
-        const { data } = await createUser({ variables: { input } });
-        setFormData([...formData, data.createPost]);
-      }
 
-      event.target.reset();
-    } catch (error) {
-      console.error("Error creating/updating post:", error);
-    } finally {
-      setTimeout(() => {
-        setShowLoader(false);
-      }, 100);
-    }
-  };
+        if (editIndex !== null) {
+          const postId = formData[editIndex].id;
+          await updateUser({ variables: { id: postId, input } });
+          const updatedFormData: any = [...formData];
+          updatedFormData[editIndex] = {
+            ...updatedFormData[editIndex],
+            ...input,
+          };
+          setFormData(updatedFormData);
+          setEditIndex(null);
+        } else {
+          const { data } = await createUser({ variables: { input } });
+          setFormData([...formData, data.createPost]);
+        }
+
+        formik.resetForm();
+      } catch (error) {
+        console.error("Error creating/updating post:", error);
+      } finally {
+        setTimeout(() => {
+          setShowLoader(false);
+        }, 100);
+      }
+    },
+  });
 
   const handleEdit = (index: any) => {
     setEditIndex(index);
     const { title, body } = formData[index];
-    const titleInput = document.getElementById("title") as HTMLInputElement;
-    const bodyInput = document.getElementById("body") as HTMLInputElement;
-    titleInput.value = title;
-    bodyInput.value = body;
+    formik.setFieldValue("title", title);
+    formik.setFieldValue("body", body);
   };
 
   const handleDelete = async (index: number) => {
@@ -89,9 +108,47 @@ const CreateUserMutation = () => {
     }
   };
 
+  // Pagination logic
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = formData.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  const filteredFormData = currentPosts.filter((item) =>
+    item.title.toLowerCase().includes(searchKeyword.toLowerCase())
+  );
+
   return (
     <div className="mainDiv">
-      <form onSubmit={handleSubmit}>
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      >
+        <IconButton sx={{ p: "10px" }} aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search By Title"
+          inputProps={{ "aria-label": "Search By Title " }}
+          id="search"
+          type="text"
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
+        />
+        <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+          {/* <SearchIcon /> */}
+        </IconButton>
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton color="primary" sx={{ p: "10px" }} aria-label="directions">
+          <DirectionsIcon />
+        </IconButton>
+      </Paper>
+      <br />
+      <br />
+
+      <form onSubmit={formik.handleSubmit}>
         <TextField
           id="title"
           type="text"
@@ -99,7 +156,10 @@ const CreateUserMutation = () => {
           variant="outlined"
           name="title"
           placeholder="Enter Title"
-          required
+          value={formik.values.title}
+          onChange={formik.handleChange}
+          error={formik.touched.title && Boolean(formik.errors.title)}
+          helperText={formik.touched.title && formik.errors.title}
         />
         <br />
         <br />
@@ -110,7 +170,10 @@ const CreateUserMutation = () => {
           type="text"
           name="body"
           placeholder="Enter Body"
-          required
+          value={formik.values.body}
+          onChange={formik.handleChange}
+          error={formik.touched.body && Boolean(formik.errors.body)}
+          helperText={formik.touched.body && formik.errors.body}
         />
         <br />
         <br />
@@ -143,10 +206,10 @@ const CreateUserMutation = () => {
 
       <div className="test1">
         <h2 style={{ display: "flex", justifyContent: "center" }}>
-          {formData.length > 0 ? <>Post Data</> : ""}
+          {filteredFormData.length > 0 ? <>Post Data</> : ""}
         </h2>
         <ul className="test1">
-          {formData.length > 0 ? (
+          {filteredFormData.length > 0 ? (
             <table border={2}>
               <thead>
                 <tr>
@@ -159,30 +222,16 @@ const CreateUserMutation = () => {
                 </tr>
               </thead>
               <tbody>
-                {formData.map((data, id) => (
-                  <tr key={id}>
-                    <td className="gap2">{id + 1}</td>
+                {filteredFormData.map((data, index) => (
+                  <tr key={index}>
+                    <td className="gap2">{indexOfFirstPost + index + 1}</td>
                     <td className="gap2">{data.title}</td>
                     <td className="gap2">{data.body}</td>
                     <td className="gap2">
-                      {/* <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={() => handleEdit(index)}
-                    >
-                      Edit
-                    </Button> */}
-                      <UpdateUserMutation onClick={() => handleEdit(id)} />
+                      <UpdateUserMutation onClick={() => handleEdit(index)} />
                     </td>
                     <td className="gap2">
-                      {/* <Button
-                      variant="contained"
-                      color="error"
-                      onClick={() => handleDelete(index)}
-                    >
-                      Delete
-                    </Button> */}
-                      <DeleteUserMutation onClick={() => handleDelete(id)} />
+                      <DeleteUserMutation onClick={() => handleDelete(index)} />
                     </td>
                   </tr>
                 ))}
@@ -194,6 +243,24 @@ const CreateUserMutation = () => {
             </div>
           )}
         </ul>
+      </div>
+
+      {/* Pagination */}
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+      >
+        <Button
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </Button>
+        <Button
+          onClick={() => paginate(currentPage + 1)}
+          disabled={currentPosts.length < postsPerPage}
+        >
+          Next
+        </Button>
       </div>
     </div>
   );
--- pages/createpostdata.tsx
+++ pages/createpostdata.tsx
@@ -1,10 +1,22 @@
 import CreateUserMutation from "@/CreateUserMutation";
 import React from "react";
 import Header from "./Components/Header/Header";
+import { useFormik } from "formik";
 
 interface Props {}
 
 const createpostdata = (props: Props) => {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      body: "",
+    },
+    onSubmit: (values) => {
+      // Your form submission logic here
+    },
+    // Add any other configuration such as validationSchema if needed
+  });
+
   return (
     <div>
       <Header />
